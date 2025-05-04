7d06b2fd6201330aac263d3388d74b8b85a4eca7
Humayun Kabir <mdhumayunkabir203@gmail.com>
2025-05-04 17:50:34 +0600

    Update logistic regression.r

--- logistic regression.r
+++ logistic regression.r
@@ -1,4 +1,4 @@
-df <- data.frame(
+df = data.frame(
    Income = c(4000, 3000, 5000, 2500, 6000, 4500, 3200, 5800, 2200, 4900),
    CreditScore = c(700, 650, 720, 600, 750, 710, 640, 730, 580, 705),
    LoanAmount = c(20000, 15000, 25000, 10000, 27000, 22000, 16000, 26000, 9000, 24000),
@@ -11,10 +11,10 @@ plot(df$Income~df$LoanAmount)
 print(df)
 
 # Create dummy variable for Married (Yes = 1, No = 0)
-df$Married_dummy <- ifelse(df$Married == "Yes", 1, 0)  # Fixed assignment operator
+df$Married_dummy = ifelse(df$Married == "Yes", 1, 0)  # Fixed assignment operator
 
 # Fit logistic regression model
-model <- glm(Approved ~ Income + CreditScore + LoanAmount + Married_dummy, 
+model = glm(Approved ~ Income + CreditScore + LoanAmount + Married_dummy, 
              family = binomial(link = "logit"), 
              data = df)
 
@@ -23,7 +23,7 @@ summary(model)
 
 
 # New applicant data
-new_applicant <- data.frame(
+new_applicant = data.frame(
    Income = 4500,
    CreditScore = 700,
    LoanAmount = 20000,
@@ -31,7 +31,7 @@ new_applicant <- data.frame(
 )
 
 # Predict probability of approval
-prob <- predict(model, newdata = new_applicant, type = "response")
+prob = predict(model, newdata = new_applicant, type = "response")
 
 # Print the probability (formatted as percentage)
 cat("Probability of loan approval:", round(prob * 100, 1), "%\n")
