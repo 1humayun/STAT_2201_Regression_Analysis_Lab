3994f851ddebf4afab93703d3c9eec74d185b640
Humayun Kabir <mdhumayunkabir203@gmail.com>
2025-05-04 17:52:49 +0600

    Update Problem 6 (ANOVA table).R

--- Problem 6 (ANOVA table).R
+++ Problem 6 (ANOVA table).R
@@ -1,29 +1,29 @@
 ########################## Problem 6 | (ANOVA Table Analysis) ###############
 # Given data
-SSR <- 260.1784  # Regression Sum of Squares
-SST <- 262.6886  # Total Sum of Squares
-df_total <- 39   # Total Degrees of Freedom
-df_error <- 34   # Error Degrees of Freedom
+SSR = 260.1784  # Regression Sum of Squares
+SST = 262.6886  # Total Sum of Squares
+df_total = 39   # Total Degrees of Freedom
+df_error = 34   # Error Degrees of Freedom
 
 # Compute SSE (Error Sum of Squares)
-SSE <- SST - SSR  # Error Sum of Squares
+SSE = SST - SSR  # Error Sum of Squares
 
 # Compute df for regression
-df_regression <- df_total - df_error  # Degrees of Freedom for Regression
+df_regression = df_total - df_error  # Degrees of Freedom for Regression
 
 # (i) Estimated standard error of regression, s
-MSE <- SSE / df_error  # Mean Square Error
-s <- sqrt(MSE)  # Standard error of regression
+MSE = SSE / df_error  # Mean Square Error
+s = sqrt(MSE)  # Standard error of regression
 
 # (ii) Coefficient of multiple determination (R^2)
-R_squared <- SSR / SST
+R_squared = SSR / SST
 
 # (iii) Adjusted R-squared
-adj_R_squared <- 1 - ((SSE / df_error) / (SST / df_total))
+adj_R_squared = 1 - ((SSE / df_error) / (SST / df_total))
 
 # (iv) F-statistic for goodness of fit
-MSR <- SSR / df_regression  # Mean Square for Regression
-F_statistic <- MSR / MSE  # F-test value
+MSR = SSR / df_regression  # Mean Square for Regression
+F_statistic = MSR / MSE  # F-test value
 
 # Print results
 cat("Estimated Standard Error (s):", s, "\n")
