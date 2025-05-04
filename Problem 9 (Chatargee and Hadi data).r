a680195a188041d97b14efb32df1bd848f4f32d3
Humayun Kabir <mdhumayunkabir203@gmail.com>
2025-05-04 17:52:03 +0600

    Update Problem 9 (Chatargee and Hadi data).r

--- Problem 9 (Chatargee and Hadi data).r
+++ Problem 9 (Chatargee and Hadi data).r
@@ -2,14 +2,14 @@
 
 
 # Define the data
-X <- c(-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7)
-Y <- c(1, 14, 25, 34, 41, 46, 49, 50, 49, 46, 41, 34, 25, 14, 1)
+X = c(-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7)
+Y = c(1, 14, 25, 34, 41, 46, 49, 50, 49, 46, 41, 34, 25, 14, 1)
 
 # Create a data frame
-df <- data.frame(X, Y)
+df = data.frame(X, Y)
 
 # Fit a linear regression model
-linear_model <- lm(Y ~ X, data = df)
+linear_model = lm(Y ~ X, data = df)
 
 # Print summary of the model
 summary(linear_model)
@@ -37,21 +37,3 @@ plot(X, residuals(linear_model),
 
 # Add quadratic regression curve
 lines(X, residuals(linear_model), col = "red", lwd = 2)
-
-
-
-
-
-
-
-
-
-
-# Fit a quadratic regression model (degree = 2)
-quadratic_model <- lm(Y ~ poly(X, 2, raw = TRUE))
-
-# Generate a sequence of X values for smooth curve
-X_seq <- seq(min(X), max(X), length.out = 100)  # More points for a smooth curve
-
-# Predict Y values using the fitted quadratic model
-Y_pred <- predict(quadratic_model, newdata = data.frame(X = X_seq))
